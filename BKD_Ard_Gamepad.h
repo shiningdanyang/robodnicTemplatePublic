29fc6747965cb699bb690129a805c4a9e1170d79
Đan Dương <35462935+shiningdanyang@users.noreply.github.com>
2024-07-30 16:47:13 +0700

    Update BKD_Ard_Gamepad.h

--- BKD_Ard_Gamepad.h
+++ BKD_Ard_Gamepad.h
@@ -1,3 +1,9 @@
+/*
+     Created on: Oct 19, 2022
+     Author: ShiningDanyang
+     Please respect the author and do not take it as your own
+*/
+
 #ifndef INC_BKD_ARD_GAMEPAD_H_
 #define INC_BKD_ARD_GAMEPAD_H_
 #include "BKD_Ard_Motor.h"
